938dcdb2f5f1ed9d4faa1e312229273f398c79c9
carlam97 <carmartins04@hotmail.com>
2021-11-03 19:20:40 +0000

    led

--- led.py
+++ led.py
@@ -1,5 +1,26 @@
+# no terminal dar o comando pip install paho-mqtt
+
+# importar bibliotecas 
+import paho.mqtt.client as mqtt
+import sys
 import RPi.GPIO as GPIO
 import time
+
+# configurações do MQTT
+Broker = "test.mosquitto.org"
+PortaBroker = 1883
+KeepAliveBroker = 60
+TopicoSubscribe = "cm031121"
+
+# Função para conexão com o broker
+def on_connect(client, userdata, flags, rc):
+    print('[STATUS] Conectando ao Broker, Resultado: ', + str(rc))
+    # Increver no tópico configurado
+    cliente.subscribe(TopicoSubscribe)
+    
+    
+
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT)
